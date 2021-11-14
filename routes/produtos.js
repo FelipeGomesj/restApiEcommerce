12b8d1df6df0d66616fd647a5280bd0b250f5f7a
FelipeGomesj <felipe_gomesj@hotmail.com>
2021-11-14 01:16:02 -0300

    Passo 7 REST API

--- routes/produtos.js
+++ routes/produtos.js
@@ -13,42 +13,58 @@ router.get('/', (req, res, next) => {
         if(error) {return res.status(500).send({error: error})}
         conn.query(
             'SELECT * FROM produtos;',
-            (error, resultado, fields) =>{
+            (error, result, fields) =>{
                 if(error){return res.status(500).send({error: error})}
-                return res.status(200).send({response:resultado})
+                const response ={
+                    quantidade: result.length,
+                    produtos: result.map(prod => {
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna os detalhes de um produto específico',
+                                url: 'http://localhost:3000/produtos/' + prod.id_produto
+
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send({response});
             }
         )
     })
 });
 
 //Insere um produto
-router.post('/', (req, res, next) => {
-    const produto = {
-        nome_produto: req.body.nome, /**Pegar o valor de nome do corpo do json  */
-        preco_produto: req.body.preco /** Pegar o valor de preço do corpo json */
-    };
-    /**Fazendo o insert no banco de dados  */
+router.post('/', (req, res, next)=>{
     mysql.getConnection((error, conn)=>{
-        if(error){ return res.status(500).send({error: error})}
+        if(error){return res.status(500).send({error:error})}
         conn.query(
-            'INSERT INTO produtos (nome, preco) VALUES (?,?)', 
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
             [req.body.nome, req.body.preco],
-            /**CALLBACK */
-            (error, resultado, field) =>{
-                conn.release(); /** Liberando a conexão para tratar erros, isso é obrigatório, pois senão tiver a  api fica travada */
-                
-                if(error){ return res.status(500).send({error: error})}
-
-                res.status(201).send({
-                    mensagem:'Produto inserido com sucesso',
-                    id_produto: resultado.insertId
-                });
-
+            (error, result, field)=>{
+                conn.release();
+                if(error){return res.status(500).send({error: error})}
+                const response = {
+                    mensagem: 'Produto inserido com sucesso',
+                    produtoCriado:{
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request:{
+                            tipo: 'POST',
+                            descricao: 'Retorna todos os produtos',
+                            url: 'http://localhost:3000/produtos/'
+                        }
+                    }
+                }
+                 return res.status(201).send({response});
             }
         )
     })
-});
-
+})
 //Retornar os dados específico de 1 produto
 
 router.get('/:id_produto', (req,res,next) =>{
@@ -57,9 +73,29 @@ router.get('/:id_produto', (req,res,next) =>{
         conn.query(
             'SELECT * FROM produtos WHERE id_produto =?;',
             [req.params.id_produto],
-            (error, resultado, fields) =>{
+            (error, result, fields) =>{
                 if(error){return res.status(500).send({error: error})}
-                return res.status(200).send({response:resultado})
+
+                if(result.length == 0){
+                    return res.status(404).send({
+                        mensagem: 'Não foi encontrado o produto com este ID'
+                    });
+                }
+                const response = {
+                    mensagem: 'Produto inserido com sucesso',
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna todos os produtos',
+                            url: 'http://localhost:3000/produtos'
+                        }
+                    }
+
+                }
+                return res.status(200).send({response})
             }
         )
     })
@@ -77,14 +113,26 @@ router.patch('/',(req, res, next) => {
             req.body.preco, 
             req.body.id_produto],
             /**CALLBACK */
-            (error, resultado, field) =>{
+            (error, result, field) =>{
                 conn.release(); /** Liberando a conexão para tratar erros, isso é obrigatório, pois senão tiver a  api fica travada */
                 
                 if(error){ return res.status(500).send({error: error})}
 
-                res.status(202).send({
-                    mensagem:'Produto alterado com sucesso'
-                });
+                const response = {
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'PATCH',
+                            descricao: 'Atualiza um produto',
+                            url: 'http://localhost:3000/produtos' + req.body.id_produto
+                        }
+                    }
+                }
+
+                return res.status(202).send(response);
 
             }
         )
@@ -103,10 +151,19 @@ router.delete('/',(req,res,next)=> {
                 conn.release(); /** Liberando a conexão para tratar erros, isso é obrigatório, pois senão tiver a  api fica travada */
                 
                 if(error){ return res.status(500).send({error: error})}
-
-                res.status(202).send({
-                    mensagem:'Produto excluído com sucesso'
-                });
+                const response = {
+                    mensagem: 'Produto removido com sucesso',
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:3000/produtos',
+                        body:{
+                            nome: 'String',
+                            preco: 'number'
+                        }
+                    }
+                }
+                return res.status(202).send(response);
 
             }
         )
